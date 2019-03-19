d4241cbd666cc2070c26d1aaa608249802fd3659
Steven Fisher <steven.fisher22@gmail.com>
2019-03-19 16:58:47 -0500

    added FileIcon component

--- src/index.js
+++ src/index.js
@@ -25,7 +25,20 @@ FileListItem.propTypes = {
     file: PropTypes.object.isRequired
 };
 
-
+function FileIcon({file}) {
+    let icon = 'fa-file-text-o';
+    if(file.type === 'folder') {
+        icon = 'fa-folder'
+    }
+    return (
+        <td className="file-icon">
+            <i className={`fa ${icon}`}/>
+        </td>
+    );
+}
+FileIcon.propTypes = {
+    file: PropTypes.object.isRequired
+};
 
 // Fake Data 
 const testFiles = [
